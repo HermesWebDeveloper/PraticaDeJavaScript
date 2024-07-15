608fbd057a4088d8da766a47a3c1397fed782576
HERMES BARBOSA  PEREIRA <HERMES6313@DIGITALCOLLEGE.LOCAL>
2024-07-15 11:12:19 -0300

    Adicionado exercícios do 3 ao 4. 4 em andamento

--- Exercicios_IFRN/static/funcoes.js
+++ Exercicios_IFRN/static/funcoes.js
@@ -3,10 +3,45 @@ Ingresso = function(nomeEvento, valorIngresso) {
     this.valorIngresso = valorIngresso
 
     this.exibirValor = function() {
-        return valorIngresso.tofixed(2)
-    }
+        return valorIngresso.tofixed(2);
+    };
 
     this.__str__ = function() {
-        return "O nome do evento é " + this.nomeEvento + " e o valor do ingresso é R$ " + this.valorIngresso
-    }
+        return "O nome do evento é " + this.nomeEvento + " e o valor do ingresso é R$ " + this.valorIngresso;
+    };
+}
+
+Retangulo = function(largura, altura) {
+    this.largura = Number(largura);
+    this.altura = Number(altura);
+
+    this.calcularPerimetro = function() {
+        return (this.largura * 2) + (this.altura * 2);
+    };
+
+    this.calcularArea = function() {
+        return this.largura * this.altura;
+    };
+}
+
+Ponto = function(x, y) {
+    this.x = Number(x);
+    this.y = Number(y);
+}
+
+Lista = function(lista) {
+    this.listaNormal = lista;
+    
+    this.listaSemRepetir = function() {
+        var lista = this.listaNormal;
+        for (i = 1; i < lista; i++) {
+            for (j = 0; j < i; j++) {
+                if (lista[i] == lista[j]) {
+                    lista[i].splice(i, 1);
+                };
+            };
+        };
+
+        return lista;
+    };
 }
--- Exercicios_IFRN/static/main.js
+++ Exercicios_IFRN/static/main.js
@@ -1,6 +1,9 @@
+// Exercício 1:
+
 let inputNomeEvento = document.getElementById("inputNomeEvento");
 let inputValorIngresso = document.getElementById("inputValorIngresso");
 let btnEnviarEx01 = document.getElementById("btnEnviarEx01");
+let outEventoIngresso__str__ = document.getElementById("outEventoIngresso__str__");
 
 let outNomeEvento = document.getElementById("outNomeEvento");
 let outValorIngresso = document.getElementById("outValorIngresso");
@@ -10,9 +13,80 @@ const ingresso = new Ingresso(inputNomeEvento.value, inputValorIngresso.value);
 function atualizarEx01() {
     ingresso.nomeEvento = inputNomeEvento.value;
     ingresso.valorIngresso = inputValorIngresso.value;
-    console.log(ingresso)
+    // console.log(ingresso)
     outNomeEvento.textContent = ingresso.nomeEvento;
     outValorIngresso.textContent = ingresso.valorIngresso;
+    outEventoIngresso__str__.textContent = ingresso.__str__();
 }
 
 btnEnviarEx01.addEventListener('click', atualizarEx01);
+
+// Exercício 2:
+
+let inputLarguraRetangulo = document.getElementById("inputLarguraRetangulo");
+let inputAlturaRetangulo = document.getElementById("inputAlturaRetangulo");
+let btnEnviarEx02 = document.getElementById("btnEnviarEx02");
+
+let outPerimetroRetangulo = document.getElementById("outPerimetroRetangulo");
+let outAreaRetangulo = document.getElementById("outAreaRetangulo");
+
+retangulo = new Retangulo(Number(inputLarguraRetangulo.value), Number(inputAlturaRetangulo.value));
+
+function atualizarEx02() {
+    retangulo.largura = Number(inputLarguraRetangulo.value);
+    retangulo.altura = Number(inputAlturaRetangulo.value);
+
+    outPerimetroRetangulo.textContent = retangulo.calcularPerimetro();
+    outAreaRetangulo.textContent = retangulo.calcularArea();
+}
+
+btnEnviarEx02.addEventListener('click', atualizarEx02);
+
+// Exercício 3:
+
+let inputPontoX = document.getElementById("inputPontoX");
+let inputPontoY = document.getElementById("inputPontoY");
+let btnEnviarEx03 = document.getElementById("btnEnviarEx03");
+let btnZerarLista = document.getElementById("btnZerarLista");
+
+let outListaPontos = document.getElementById("outListaPontos");
+
+let ponto = new Ponto(Number(inputPontoX.value), Number(inputPontoY.value));
+let listaPontos = []
+listaPontos[0] = ponto
+
+function atualizarEx03 () {
+    ponto.x = Number(inputPontoX.value);
+    ponto.y = Number(inputPontoY.value);
+    listaPontos[listaPontos.length] = ponto;
+
+    outListaPontos.innerHTML += `<li> Ponto ${listaPontos.length-1}: (${ponto.x}, ${ponto.y}) </li>`;
+}
+
+function zerarLista() {
+    listaPontos = [];
+    outListaPontos.innerHTML = ""
+}
+
+btnEnviarEx03.addEventListener('click', atualizarEx03);
+btnZerarLista.addEventListener('click', zerarLista);
+
+// Exercício 4:
+
+let inputElementoLista = document.getElementById("inputElementoLista");
+let btnAdicionarElemeto = document.getElementById("btnAdicionarElemeto");
+
+let outLista = document.getElementById("outLista");
+let outListaSemRepetir = document.getElementById("outListaSemRepetir");
+
+let array = [];
+let lista = new Lista(array);
+
+function atualizarEx04() {
+    lista.listaNormal.push(String(inputElementoLista.value))
+    outLista.innerHTML = `[${lista.listaNormal}]`
+
+    outListaSemRepetir.innerHTML = `[${lista.listaSemRepetir()}]`
+}
+
+btnAdicionarElemeto.addEventListener('click', atualizarEx04);
